6d66565f2a60078d64d1a3fb9dd6dd6c8448ea36
Petr Semiletov <peter.semiletov@gmail.com>
2025-04-08 15:42:39 +0300

    4.0.0 + progress bars

--- drumlabooh-net-install.go
+++ drumlabooh-net-install.go
@@ -1,11 +1,9 @@
 package main
 
-
 import (
     "fmt"
     "os"
     "net/http"
-    //"strconv"
     "os/user"
     "io"
     "log"
@@ -15,37 +13,26 @@ import (
     "path/filepath"
 )
 
-
-
-func read_url_as_string (url string) string {
-	
-	req, err := http.NewRequest ("GET", url, nil)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	req.Header.Set ("Accept", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do (req)
-	if err != nil {
-		log.Fatalln (err)
-	}
-
-	defer resp.Body.Close()
-
-	b, err := io.ReadAll (resp.Body)
-	if err != nil {
-		log.Fatalln (err)
-	}
-
-	return (string(b))
-	
+func read_url_as_string(url string) string {
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        log.Fatalln(err)
+    }
+    req.Header.Set("Accept", "application/json")
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        log.Fatalln(err)
+    }
+    defer resp.Body.Close()
+    b, err := io.ReadAll(resp.Body)
+    if err != nil {
+        log.Fatalln(err)
+    }
+    return string(b)
 }
 
-
 func userHomeDir() string {
-    
     if runtime.GOOS == "windows" {
         home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
         if home == "" {
@@ -58,71 +45,139 @@ func userHomeDir() string {
             return home
         }
     }
-    
     return os.Getenv("HOME")
 }
 
+type ProgressReader struct {
+    Reader    io.Reader
+    Total     int64
+    Downloaded int64
+    Name      string
+}
 
-func Unzip (src, dest, ver string) error {
+func (pr *ProgressReader) Read(p []byte) (n int, err error) {
+    n, err = pr.Reader.Read(p)
+    pr.Downloaded += int64(n)
     
-  //  DRUMSKLAD_DIR := "drum_sklad-1.0.0"
-    dest = filepath.Clean (dest) + string (os.PathSeparator)
+    // Проверка на случай, если Total <= 0
+    var percent float64
+    if pr.Total > 0 {
+        percent = float64(pr.Downloaded) / float64(pr.Total) * 100
+    }
+    
+    // Ограничиваем percent от 0 до 100
+    if percent > 100 {
+        percent = 100
+    }
+    if percent < 0 {
+        percent = 0
+    }
+    
+    barLength := int(percent / 2)
+    if barLength < 0 {
+        barLength = 0
+    }
+    
+    fmt.Printf("\rDownloading %s: [%-50s] %.2f%%", 
+        pr.Name,
+        strings.Repeat("#", barLength),
+        percent)
+    if pr.Downloaded >= pr.Total && pr.Total > 0 {
+        fmt.Println()
+    }
+    return
+}
 
-    r, err := zip.OpenReader (src)
+func downloadFile(filepath string, url string) error {
+    out, err := os.Create(filepath)
     if err != nil {
         return err
     }
-    defer func() {
-        if err := r.Close(); err != nil {
-            panic (err)
-        }
-    }()
+    defer out.Close()
 
-    //os.MkdirAll (dest, 0755)
-    os.MkdirAll (dest, os.ModePerm)
-    
-    
+    resp, err := http.Get(url)
+    if err != nil {
+        return err
+    }
+    defer resp.Body.Close()
 
-    // Closure to address file descriptors issue with all the deferred .Close() methods
+    totalSize := resp.ContentLength
+    progressReader := &ProgressReader{
+        Reader:    resp.Body,
+        Total:     totalSize,
+        Name:      filepath[strings.LastIndex(filepath, "/")+1:],
+    }
+    
+    _, err = io.Copy(out, progressReader)
+    if err != nil {
+        return err
+    }
+    return nil
+}
+
+func Unzip(src, dest, ver string) error {
+    dest = filepath.Clean(dest) + string(os.PathSeparator)
+    r, err := zip.OpenReader(src)
+    if err != nil {
+        return err
+    }
+    defer r.Close()
+
+    os.MkdirAll(dest, os.ModePerm)
+    
+    totalFiles := len(r.File)
+    currentFile := 0
+
     extractAndWriteFile := func(f *zip.File) error {
-        path := filepath.Join (dest, f.Name)
-        // Check for ZipSlip: https://snyk.io/research/zip-slip-vulnerability
-        if ! strings.HasPrefix (path, dest) {
-            return fmt.Errorf ("%s: illegal file path", path)
+        currentFile++
+        path := filepath.Join(dest, f.Name)
+        if !strings.HasPrefix(path, dest) {
+            return fmt.Errorf("%s: illegal file path", path)
         }
-
-        path = strings.Replace (path, "drum_sklad-" + ver, "drum_sklad", 1)
- 
+        
+        path = strings.Replace(path, "drum_sklad-"+ver, "drum_sklad", 1)
         rc, err := f.Open()
         if err != nil {
             return err
         }
+        defer rc.Close()
+
+        // Проверка на случай, если totalFiles == 0
+        var percent float64
+        if totalFiles > 0 {
+            percent = float64(currentFile) / float64(totalFiles) * 100
+        }
         
-        defer func() {
-            if err := rc.Close(); err != nil {
-                panic(err)
-            }
-        }()
+        // Ограничиваем percent от 0 до 100
+        if percent > 100 {
+            percent = 100
+        }
+        if percent < 0 {
+            percent = 0
+        }
+        
+        barLength := int(percent / 2)
+        if barLength < 0 {
+            barLength = 0
+        }
+
+        fmt.Printf("\rUnpacking %s: [%-50s] %.2f%% (%d/%d)", 
+            src[strings.LastIndex(src, "/")+1:],
+            strings.Repeat("#", barLength),
+            percent,
+            currentFile,
+            totalFiles)
 
         if f.FileInfo().IsDir() {
-//                os.MkdirAll (path, f.Mode())
-            os.MkdirAll (path, os.ModePerm)
-            
-
+            os.MkdirAll(path, os.ModePerm)
         } else {
-            os.MkdirAll (filepath.Dir(path), os.ModePerm)
-            
-            f, err := os.OpenFile (path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+            os.MkdirAll(filepath.Dir(path), os.ModePerm)
+            outFile, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
             if err != nil {
                 return err
             }
-            defer func() {
-                if err := f.Close(); err != nil {
-                    panic (err)
-                }
-            }()
-
-            _, err = io.Copy(f, rc)
+            defer outFile.Close()
+            _, err = io.Copy(outFile, rc)
             if err != nil {
                 return err
             }
@@ -131,210 +186,114 @@ func Unzip (src, dest, ver string) error {
     }
 
     for _, f := range r.File {
-        err := extractAndWriteFile(f)
-        if err != nil {
+        if err := extractAndWriteFile(f); err != nil {
             return err
         }
     }
-
+    fmt.Println()
     return nil
 }
 
-
-func downloadFile (filepath string, url string) (err error) {
-
-  // Create the file
-  out, err := os.Create(filepath)
-  if err != nil  {
-    return err
-  }
-  defer out.Close()
-
-  // Get the data
-  resp, err := http.Get(url)
-  if err != nil {
-    return err
-  }
-  defer resp.Body.Close()
-
-  // Writer the body to file
-  _, err = io.Copy(out, resp.Body)
-  if err != nil  {
-    return err
-  }
-
-  return nil
+func isRoot() bool {
+    currentUser, err := user.Current()
+    if err != nil {
+        log.Fatalf("[isRoot] Unable to get current user: %s", err)
+    }
+    return currentUser.Username == "root"
 }
 
-
-func isRoot() bool { 
-    currentUser, err := user.Current() 
-    
-    if err != nil { log.Fatalf("[isRoot] Unable to get current user: %s", err) } 
-    return currentUser.Username == "root"  
-}    
-    //{ fmt.Printf("Am I root? %v", isRoot())
-    
-
 func main() {
-    
-    VER_LOCAL :="3" 
+    VER_LOCAL := "4"
     
     if isRoot() {
-        fmt.Println ("Please run as non-root")
+        fmt.Println("Please run as non-root")
         return
     }
         
-    
     var arg string
-    
     if len(os.Args) == 2 {
         arg = os.Args[1]
-        //fmt.Println ("AAAAAAAAAAAA")
-    }   
-    
+    }
     
     flag_test := false
-    
-    if (arg == "test"){
+    if arg == "test" {
         flag_test = true
-        fmt.Println ("TEST")
-    }    
-	//fmt.Println (exe_path)
-    
-//    ver_remote := read_url_as_string ("https://raw.githubusercontent.com/psemiletov/drumlabooh-net-install/refs/heads/main/version.txt")
-    
- /*   version_remote, err := strconv.Atoi(ver_remote)
-    if err != nil {
-        // ... handle error
-        panic(err)
-    } 
-   */ 
- 
- 
- /*   
-    if (ver_remote == VER_LOCAL){
-        fmt.Println ("Installer version is up-to-date")
-    } else {
-            fmt.Println ("Installer is updating... Wait")
+        fmt.Println("TEST")
+    }
         
-            exe_path, err := os.Executable()
-
-            if err != nil {
-	  	       log.Fatalln(err)
-               fmt.Println (exe_path)
-            }
-            //installer_url := 
-            
-           // downloadFile (exe_path, installer_url)
-
-            //UPDATE BINARY HERE!
-            
-    
-	        
-	
-        
-           fmt.Println ("Done")
-           fmt.Println ("Please restart Installer")
-        
-           return
-         }
-        
-*/
-        
-    fmt.Println ("Drumlabooh Net Installer " + VER_LOCAL)
-    
+    fmt.Println("Drumlabooh Net Installer " + VER_LOCAL)
     
     home_dir, err := os.UserHomeDir()
     if err != nil {
-        log.Fatal (err)
+        log.Fatal(err)
     }
     
-    fmt.Println ("home_dir:" + home_dir)
+    fmt.Println("home_dir:" + home_dir)
     
-    
-    tempdir, err := os.MkdirTemp ("", "laboohtempdir")
-
+    tempdir, err := os.MkdirTemp("", "laboohtempdir")
     if err != nil {
-        log.Fatal (err)
+        log.Fatal(err)
     }
     
+    fmt.Println("Temp dir name:", tempdir)
     
-    fmt.Println ("Temp dir name:", tempdir)
-    
-    labooh_ver := read_url_as_string ("https://raw.githubusercontent.com/psemiletov/drumlabooh/refs/heads/main/version.txt")
-    kits_ver := read_url_as_string ("https://raw.githubusercontent.com/psemiletov/drum_sklad/refs/heads/main/version.txt")
+    labooh_ver := read_url_as_string("https://raw.githubusercontent.com/psemiletov/drumlabooh/refs/heads/main/version.txt")
+    kits_ver := read_url_as_string("https://raw.githubusercontent.com/psemiletov/drum_sklad/refs/heads/main/version.txt")
 
-    fmt.Println ("Install/update Drumlabooh v.", labooh_ver)
-    fmt.Println ("Install/update kits v.", kits_ver)
+    fmt.Println("Install/update Drumlabooh v.", labooh_ver)
+    fmt.Println("Install/update kits v.", kits_ver)
     
     lv2_url := "https://github.com/psemiletov/drumlabooh/releases/download/" + labooh_ver + "/drumlabooh.lv2.zip"
     vst_url := "https://github.com/psemiletov/drumlabooh/releases/download/" + labooh_ver + "/drumlabooh.vst3.zip"
-    
-    
-    drumkits_url := "https://github.com/psemiletov/drum_sklad/archive/refs/tags/"+ kits_ver + ".zip"
-    
-    fmt.Println ("drumkits_url: " + drumkits_url)
-    fmt.Println ("lv2_url: " + lv2_url)
-    fmt.Println ("vst_url: " + vst_url)
-    
-    
+    drumkits_url := "https://github.com/psemiletov/drum_sklad/archive/refs/tags/" + kits_ver + ".zip"
     
     source_path_to_lv2_zip := tempdir + "/labooh_lv2.zip"
     source_path_to_vst_zip := tempdir + "/labooh_vst.zip"
     source_path_to_drum_sklad := tempdir + "/drum_sklad.zip"
     
-    
     dest_lv2_path := home_dir + "/.lv2"
     dest_vst_path := home_dir + "/.vst3"
-    dest_drumsklad_path := home_dir;
+    dest_drumsklad_path := home_dir
+    
+    if (flag_test){
+        dest_lv2_path := dest_lv2_path + "TEST" 
+        dest_vst_path := dest_vst_path + "TEST"
+        dest_drumsklad_path := "/STEST"
+    }
     
     
-    fmt.Println ("Downloading LV2 to " + source_path_to_lv2_zip)
-    if (! flag_test){
-        downloadFile (source_path_to_lv2_zip, lv2_url)
-    } 
+    fmt.Println("Downloading LV2 to " + source_path_to_lv2_zip)
+    if !flag_test {
+        downloadFile(source_path_to_lv2_zip, lv2_url)
+    }
     
-    fmt.Println ("Downloading VST3i to " + source_path_to_vst_zip)
-    if (! flag_test){
-        downloadFile (source_path_to_vst_zip, vst_url)
-    } 
+    fmt.Println("Downloading VST3i to " + source_path_to_vst_zip)
+    if !flag_test {
+        downloadFile(source_path_to_vst_zip, vst_url)
+    }
 
-    fmt.Println ("Downloading kits to " + source_path_to_drum_sklad)
+    fmt.Println("Downloading kits to " + source_path_to_drum_sklad)
+    if !flag_test {
+        downloadFile(source_path_to_drum_sklad, drumkits_url)
+    }
 
-    if (! flag_test){
-       downloadFile (source_path_to_drum_sklad, drumkits_url)
-    }   
-
+    fmt.Println("Unpacking LV2 from " + source_path_to_lv2_zip)
+    fmt.Println("Unpacking LV2 to " + dest_lv2_path)
+    if !flag_test {
+        Unzip(source_path_to_lv2_zip, dest_lv2_path, kits_ver)
+    }
     
-    //downloadFile("5.0.0.zip", "https://github.com/psemiletov/drumlabooh/archive/refs/tags/5.0.0.zip")
-    //Unzip("5.0.0.zip", ".lv2/")
+    fmt.Println("Unpacking VST3i from " + source_path_to_vst_zip)
+    fmt.Println("Unpacking VST3i to " + dest_vst_path)
+    if !flag_test {
+        Unzip(source_path_to_vst_zip, dest_vst_path, kits_ver)
+    }
 
-    fmt.Println ("Unpacking LV2 from " + source_path_to_lv2_zip)
-    fmt.Println ("Unpacking LV2 to " + dest_lv2_path)
-       
-    
-
-    if (! flag_test){
-         Unzip (source_path_to_lv2_zip, dest_lv2_path, kits_ver)
-    } 
-    
-    fmt.Println ("Unpacking VST3i from " + source_path_to_vst_zip)
-    fmt.Println ("Unpacking VST3i to " + dest_vst_path)
-    
-    if (! flag_test){
-        Unzip (source_path_to_vst_zip, dest_vst_path, kits_ver)
-    } 
-
-    fmt.Println ("Unpacking drumkits to subdir drum_sklad at " + dest_drumsklad_path + "/drum_sklad")
-
-    if (! flag_test) {
-       Unzip (source_path_to_drum_sklad, dest_drumsklad_path, kits_ver)
+    fmt.Println("Unpacking drumkits to subdir drum_sklad at " + dest_drumsklad_path + "/drum_sklad")
+    if !flag_test {
+        Unzip(source_path_to_drum_sklad, dest_drumsklad_path, kits_ver)
     }
    
-    //remove all archives
-    fmt.Println ("Removing temp dir: " + tempdir)
-    
-    os.RemoveAll (tempdir)
-    
+    fmt.Println("Removing temp dir: " + tempdir)
+    os.RemoveAll(tempdir)
 }
